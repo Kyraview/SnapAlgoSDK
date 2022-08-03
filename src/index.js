f2609ca165551f45941ede9ce473ddad0013b7ca
paul fears <paulrfears@gmail.com>
2022-08-03 09:03:25 -0500

    export change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import HTTPClient from './HTTPClient.js';
 const IPFSURL = 'https://snapalgo-imgs.netlify.app/imgs' 
 const importIcon = IPFSURL + '/import-wallet.svg'
 const connectedGif = IPFSURL + '/connected.gif'
-export default class Wallet{
+export class Wallet{
     constructor(){
       this.enabled = false;
       this.genisisHash = null;
